d1de0348f73274f866908f79c1833fc958216fbd
Akyl <kurmanalievakyl5@gmail.com>
2023-03-29 20:20:16 +0600

    practise 2

---
+++ lesson2.dart
@@ -0,0 +1,73 @@
+
+void main() {
+  num a = 1;
+  num b = 2;
+  num c = 3;
+  num d = 4;
+  num e = 5;
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+  print(e);
+
+  int number1 = 10;
+  int number2 = 20;
+  int number3 = 30;
+  int number4 = 40;
+  int number5 = 50;
+  print(number1);
+  print(number2);
+  print(number3);
+  print(number4);
+  print(number5);
+
+  double a2 = 3.4;
+  double b2 = 4.5;
+  double c2 = 5.6;
+  double d2 = 6.7;
+  double e2 = 7.8;
+  print(a2);
+  print(b2);
+  print(c2);
+  print(d2);
+  print(e2);
+
+
+  String name = "Akyl";
+  String surname = "Kurmanaliev";
+  String laptop = "ACER";
+  String city = "Bishkek";
+  String mouse = "Bloody";
+  print(name);
+  print(surname);
+  print(laptop);
+  print(city);
+  print(mouse);
+
+  List name_of_schools = [ "Ilim", "AUCA", "Bilim", "Harvard", "Cambridge"];
+  print(name_of_schools);
+
+  Map company = {
+    'name of course': 'ITCBootcamp',
+    'students': 10,
+    'typeOfCourse': 'Flutter',
+    'location': 'Bishkek city',
+  };
+  print(company);
+
+  var roman_numeral_one = '\u215A';
+  print(roman_numeral_one);
+
+  final integer = 120;
+  print(integer);
+
+  const integer2 = 230;
+  print(integer2);
+
+  dynamic integer3 = 460;
+  print(integer3);
+
+  var integer4 = 580;
+  print(integer4);
+}
